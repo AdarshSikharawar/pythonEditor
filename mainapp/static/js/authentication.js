87e9fbf59aef2b24a9cad06df5312699b669d2e6
Mohd Shahnawaz Ansari <snawaz9770@gmail.com>
2025-09-30 15:03:29 +0530

    Made code changes and removed extra folder

---
+++ mainapp/static/js/authentication.js
@@ -0,0 +1,35 @@
+function toggleForm(formId) {
+    // Hide all forms
+    document.querySelectorAll('.form-section').forEach(form => {
+        form.classList.remove('active');
+    });
+
+    // Show selected form
+    document.getElementById(formId).classList.add('active');
+
+}
+
+setTimeout(function () {
+    const messages = document.querySelectorAll('.message');
+    messages.forEach(msg => msg.style.display = 'none');
+}, 3000);
+
+function validate() {
+    let password = document.getElementById("password").value.trim();
+    let confirmPassword = document.getElementById("confirm-password").value.trim();
+    let message = document.getElementById("msg");
+    
+    message.textContent = "";
+
+    if (confirmPassword === "") {
+        return;
+    }
+    
+    if (password === confirmPassword) {
+        message.style.color = "green";
+        message.textContent = "Password matched";
+    } else {
+        message.style.color = "red";
+        message.textContent = "Password and confirm password must be same";
+    }
+}
