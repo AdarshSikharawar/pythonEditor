36c0a50ee9871960c3b371ed246e5a462432d2ea
Mohd Shahnawaz Ansari <snawaz9770@gmail.com>
2026-06-19 02:05:32 +0530

    Update settings.py

--- project/pythonEditor/pythonEditor/settings.py
+++ project/pythonEditor/pythonEditor/settings.py
@@ -207,8 +207,9 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = 'pygenixeditor@gmail.com'  
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 
